ad12ed26244752cdfe6f594918b9f1f561ddbd5f
Jakub Gajda <kubus.pospolitos@gmail.com>
2026-02-07 10:10:40 +0100

    Create zadanie_metody.py

---
+++ zadanie_metody.py
@@ -0,0 +1,9 @@
+Napisz metodę oblicz_bmi(waga, wzrost), która:
+
+Przyjmuje wagę w kilogramach i wzrost w metrach
+Zwraca wartość BMI (BMI = waga / wzrost²)
+
+BMI < 18.5: "Niedowaga"
+18.5 ≤ BMI < 25: "Waga prawidłowa"
+25 ≤ BMI < 30: "Nadwaga"
+BMI ≥ 30: "Otyłość"
