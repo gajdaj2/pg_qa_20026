a1b2e9d78ee59f1f2c4d882ab6d1a704522832af
Jakub Gajda <kubus.pospolitos@gmail.com>
2026-02-07 16:10:38 +0100

    Create zadanie_domowe.py

---
+++ zadanie_domowe.py
@@ -0,0 +1,22 @@
+## generator danych
+#Klasa Generator
+
+#Paramtery konstrukotra
+#nazwa_kraju
+#ilosc_danych
+
+# pobierz_dane ma generowac liste danych do testów
+
+
+#Przykład
+#gen = Generator("pl_PL",20)
+
+#lista_danych = gen.pobierz_dane()
+
+#Klasa Osoba:
+#paramtery: do wyboru
+
+#[Osoba1,Osoba2,Osoba3]
+
+
+
